6ada6d045172eae2621bbc5c979736b762602b1b
peter <hguanqi7@gmail.com>
2021-08-11 11:57:41 +0800

    Added material Swapper

--- RapidPrototype2/Assets/Scripts/WaterFunc.cs
+++ RapidPrototype2/Assets/Scripts/WaterFunc.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WaterFunc : MonoBehaviour
 {
     public int CubeState = 1;
-
+    public Material WaterM, IceM, HeatM;
+    public GameObject waterSelf;
     private void FixedUpdate()
     {
        
@@ -19,10 +21,12 @@ public class WaterFunc : MonoBehaviour
             if (collision.gameObject.GetComponent<WaterFunc>().CubeState == 2) //Ice form
             {
                 CubeState = 2;
+                waterSelf.GetComponent<Renderer>().material = IceM;
             }
             else if (collision.gameObject.GetComponent<WaterFunc>().CubeState == 3) //Heat form
             {
                 CubeState = 3;
+                waterSelf.GetComponent<Renderer>().material = HeatM;
             }
             else
             {

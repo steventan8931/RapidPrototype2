89c35b0af8fd6c803993b5ef1a59d7b715b48b11
peter <hguanqi7@gmail.com>
2021-08-16 09:09:02 +0800

    Menu Template

---
+++ RapidPrototype2/Assets/menuscr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuscr : MonoBehaviour
+{
+    GameObject startbutton, exitbutton;
+    public Animator transistion;
+    public void StartGame()
+    {
+        transistion.SetTrigger("IsStart");
+        print("trigger activated");
+        //Invoke("loadGameScene", 0.5f);
+    }
+     
+    public void loadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

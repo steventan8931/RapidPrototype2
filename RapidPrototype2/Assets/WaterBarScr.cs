92498713e18cad24e12e3a140ace145b8afa7be9
peter <hguanqi7@gmail.com>
2021-08-17 16:27:05 +0800

    Optimised player Hud

--- RapidPrototype2/Assets/WaterBarScr.cs
+++ RapidPrototype2/Assets/WaterBarScr.cs
@@ -6,5 +6,16 @@ public class WaterBarScr : MonoBehaviour
 {
     public Image WaterBar;
     public float currentWater;
-    public float maxWater = 100f;
+    public float maxWater = 1000f;
+    public Gun playerGun;
+    private void Start()
+    {
+        WaterBar = GetComponent<Image>();
+
+    }
+    private void Update()
+    {
+        currentWater = playerGun.m_CurrentWater;
+        WaterBar.fillAmount = currentWater / maxWater;
+    }
 }

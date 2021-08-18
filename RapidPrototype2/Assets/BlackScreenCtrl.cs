d383c751cfed4c972b47cb5f0d3edf83452167a3
peter <hguanqi7@gmail.com>
2021-08-18 10:04:56 +0800

    Win Black Screen

---
+++ RapidPrototype2/Assets/BlackScreenCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackScreenCtrl : MonoBehaviour
+{
+    public GameObject manager, wintext;
+    public Animator black_start;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(manager.GetComponent<GameState>().m_GameWin == true)
+        {
+            black_start.SetBool("IsStart", true);
+            wintext.SetActive(true);
+        }    
+    }
+}

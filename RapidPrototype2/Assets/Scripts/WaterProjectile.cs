3e811b1ee2b1d398954208325a001d1341281445
Steven Tan <steven.tan8931@gmail.com>
2021-08-18 17:16:23 +1200

    WaterBullet Destory on wall hit, Repositioned spawners to fit this

--- RapidPrototype2/Assets/Scripts/WaterProjectile.cs
+++ RapidPrototype2/Assets/Scripts/WaterProjectile.cs
@@ -8,13 +8,21 @@ public class WaterProjectile : MonoBehaviour
     public float m_LifeTime = 0.0f;
     public float m_MaxLifeTime = 5.0f;
 
-    public LayerMask m_EnvironmentLayer;
+    public LayerMask m_HitLayerMask;
+    public float m_Radius = 0.1f;
     private void Update()
     {
         if (m_LifeTime < m_MaxLifeTime)
         {
             transform.Translate(transform.forward * m_ProjectileSpeed * Time.deltaTime, transform.parent);
             m_LifeTime += Time.deltaTime;
+
+            RaycastHit hit;
+            Vector3 step = transform.forward * m_ProjectileSpeed * Time.fixedDeltaTime;
+            if (Physics.SphereCast(transform.position, m_Radius, step.normalized, out hit, step.magnitude, m_HitLayerMask))
+            {
+                Destroy(gameObject);
+            }
         }
         else
         {
@@ -22,26 +30,5 @@ public class WaterProjectile : MonoBehaviour
         }
     }
 
-    //Destory if hits an object that it isnt a water projectile
-    private void OnCollisionEnter(Collision _collision)
-    {
-        if (_collision.gameObject.layer == 9)
-        {
-            Destroy(gameObject);
-        }
-    }
-    private void OnCollisionStay(Collision _collision)
-    {
-        if (_collision.gameObject.layer == 9)
-        {
-            Destroy(gameObject);
-        }
-    }
-    private void OnCollisionExit(Collision _collision)
-    {
-        if (_collision.gameObject.layer == 9)
-        {
-            Destroy(gameObject);
-        }
-    }
+
 }

99204fb1d5fc37e16587c11ef69ebe1470f689ac
peter <hguanqi7@gmail.com>
2021-08-18 12:57:07 +0800

    Win Black Screen

---
+++ RapidPrototype2/Assets/WinManage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WinManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject manager, winText;
+    public Animator Black_Screen;
+
+    private void FixedUpdate()
+    {
+        if(manager.GetComponent<GameState>().m_GameWin == true)
+        {
+            winText.SetActive(true);
+            Black_Screen.SetBool("IsWin", true);
+            Invoke("loadEndScene", 2.0f);
+        }
+
+    }
+
+    public void loadEndScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+}

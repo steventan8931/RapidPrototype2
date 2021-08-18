1e59c9b6519d1c726d18a0f42317ecaed952f9fe
Steven Tan <steven.tan8931@gmail.com>
2021-08-18 23:52:47 +1200

    Added Ship for End + Ship Bob

---
+++ RapidPrototype2/Assets/Scripts/ShipBob.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBob : MonoBehaviour
+{
+    public float m_BobSpeed = 5.0f;
+    public float m_YOrigin = 3.95f;
+    private void Update()
+    {
+        float YPos = Mathf.Sin(Time.time * m_BobSpeed) + m_YOrigin;
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(transform.position.x, YPos, transform.position.z);
+    }
+}
